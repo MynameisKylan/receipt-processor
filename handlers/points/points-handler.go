502e844a11f0a10c25e18b7bbce706bea095418d
Kylan Wong <60022306+MynameisKylan@users.noreply.github.com>
2025-04-15 19:48:40 -0700

    implement points handler, also switch to mux because native router isn't matching with path vars

--- handlers/points/points-handler.go
+++ handlers/points/points-handler.go
@@ -1,6 +1,35 @@
 package points
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// in-memory store for points.
+// would replace with a database in a production app
+var pointsStore = map[string]int{
+	"abc": 100,
+}
+
+type Receipt struct {
+	Retailer     string  `json:"retailer"`
+	PurchaseDate string  `json:"purchaseDate"`
+	PurchaseTime string  `json:"purchaseTime"`
+	Items        []Item  `json:"items"`
+	Total        float64 `json:"total"`
+}
+
+type Item struct {
+	ShortDescription string  `json:"shortDescription"`
+	Price            float64 `json:"price"`
+}
+
+type PointsResponse struct {
+	Points int `json:"points"`
+}
 
 func ProcessHandler(w http.ResponseWriter, r *http.Request) {
 	// todo: unmarshal receipt json, generate id, calculate points, save to memory
@@ -10,7 +39,30 @@ func ProcessHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func PointsHandler(w http.ResponseWriter, r *http.Request) {
-	// todo: return points for the given receipt id
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Points retrieved successfully"))
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Println("ID extracted:", id)
+
+	if id != "" {
+		points, exists := pointsStore[id]
+		if !exists {
+			http.Error(w, "Receipt ID not found", http.StatusNotFound)
+			return
+		}
+
+		response := PointsResponse{Points: points}
+		marshaledData, err := json.Marshal(response)
+		if err != nil {
+			http.Error(w, "Error marshaling response", http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(marshaledData)
+		return
+	}
+
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte("Invalid receipt ID"))
 }
--- main.go
+++ main.go
@@ -5,18 +5,19 @@ import (
 	"net/http"
 
 	"github.com/fetch/receipt-processor/handlers/points"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	fmt.Println("Initializing receipt processor service...")
-	mux := http.NewServeMux()
+	r := mux.NewRouter()
 
-	// routes
-	mux.HandleFunc("/receipts/process", points.ProcessHandler)
-	mux.HandleFunc("/receipts/{id}/points", points.PointsHandler)
+	// routes with HTTP methods specified
+	r.HandleFunc("/receipts/process", points.ProcessHandler)
+	r.HandleFunc("/receipts/{id}/points", points.PointsHandler)
 
 	fmt.Println("Server starting on :8181")
-	err := http.ListenAndServe(":8181", mux)
+	err := http.ListenAndServe(":8181", r)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
